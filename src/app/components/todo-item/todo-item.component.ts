1b73666d7fa09667e49791c0722ca459c679d008
Daniel Candeira <danielbarr16@gmail.com>
2022-07-17 09:52:26 -0300

    feat: add todo-item component and model

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { TodoItem } from '../../models/todo-item';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.scss'],
+  host: { class: 'todo' },
+})
+export class TodoItemComponent {
+  @Input()
+  todo: TodoItem;
+
+  @Output()
+  done = new EventEmitter<boolean>();
+
+  @Output()
+  delete = new EventEmitter<string>();
+
+  onChange(): void {
+    this.done.emit(!this.todo.done);
+  }
+
+  deleteTodo(): void {
+    this.delete.emit(this.todo.id);
+  }
+}
---
+++ src/app/models/todo-item.ts
@@ -0,0 +1,5 @@
+export interface TodoItem {
+  id: string;
+  todo: string;
+  done: boolean;
+}
