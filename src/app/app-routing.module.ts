cba8a7c1b70d4a7e12df3629e86ff5e8514df2b5
Daniel Candeira <danielbarr16@gmail.com>
2022-07-18 21:17:45 -0300

    feat: remove services and routes not used

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,11 @@ import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
-import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
-  { path: 'register', component: RegisterComponent },
   // for now routes not registered will redirect the user to the Home
   // ideally we should have a 404 page
   { path: '**', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
-import { RegisterComponent } from './register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    LogoutComponent,
-    RegisterComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, LoginComponent, LogoutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
