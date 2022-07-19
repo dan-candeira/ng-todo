bd1e5ebeea3467e056a09e2496659da99621ac6a
Daniel Candeira <danielbarr16@gmail.com>
2022-07-18 21:12:09 -0300

    feat: add guards

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(public router: Router, public auth: AuthService) {}
+
+  canActivate(): boolean {
+    if (!this.auth.isAuthenticated()) {
+      this.router.navigate(['login']);
+      return false;
+    }
+    return true;
+  }
+}
