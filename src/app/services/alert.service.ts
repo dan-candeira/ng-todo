7c445f9b79edd1322f7ae99785db3e0524b40866
Daniel Candeira <danielbarr16@gmail.com>
2022-07-18 22:18:10 -0300

    feat: add timeout to clear alert content

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -4,13 +4,21 @@ import { Alert } from '../models/alert';
 
 @Injectable({ providedIn: 'root' })
 export class AlertService {
-  alert$: Subject<Alert> = new Subject();
+  alert$: Subject<Alert | null> = new Subject();
 
   setAlert(alert: Alert): void {
     this.alert$.next(alert);
+
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
   }
 
-  getAlert(): Observable<Alert> {
+  getAlert(): Observable<Alert | null> {
     return this.alert$.asObservable();
+  }
+
+  clearAlert(): void {
+    this.alert$.next(null);
   }
 }
